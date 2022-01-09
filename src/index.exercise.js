158b93b563422e9d7669f69c6161617deb4c2de9
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-09 17:18:01 +0200

    01 - Use @reach/dialog

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,61 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Logo} from './components/logo';
+import {Dialog} from "@reach/dialog";
+import "@reach/dialog/styles.css";
+import VisuallyHidden from "@reach/visually-hidden";
+
+function ModalCustomDialog({modalOpen = 'none', onClose}) {
+    if (modalOpen === 'none') {
+        return (
+            <Dialog isOpen={false}>
+            </Dialog>
+        )
+    } else if (modalOpen === 'login') {
+        return (
+            <Dialog isOpen={true} onDismiss={onClose}>
+                <button className="close-button" onClick={onClose}><VisuallyHidden>Close</VisuallyHidden> <span
+                    aria-hidden>×</span></button>
+                <h3>LOGIN</h3>
+            </Dialog>
+        )
+    } else if (modalOpen === 'register') {
+        return (
+            <Dialog isOpen={true} onDismiss={onClose}>
+                <button className="close-button" onClick={onClose}><VisuallyHidden>Close</VisuallyHidden> <span
+                    aria-hidden>×</span></button>
+                <h3>REGISTER</h3>
+            </Dialog>
+        )
+
+    } else {
+        throw new Error('unsupported modal for custom dialog')
+    }
+}
 
 function App() {
+    const [modalOpen, setModalOpen] = React.useState('none')
+    
     return (
         <>
             <Logo width={80} height={80}/>
             <h1>Bookshelf</h1>
             <div>
-                <button onClick={() => alert('login clicked')}>Login</button>
+                <button onClick={() => {
+                    setModalOpen('login')
+                }}>Login
+                </button>
             </div>
             <div>
-                <button onClick={() => alert('register clicked')}>Register</button>
+                <button onClick={() => {
+                    setModalOpen('register')
+                }}>Register
+                </button>
             </div>
+
+            <ModalCustomDialog modalOpen={modalOpen} onClose={() => {
+                setModalOpen('none')
+            }}/>
         </>
     )
 }
