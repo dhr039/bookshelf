913487e58cfe75b123c9f3de4c3d47289b873d5e
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-10 22:15:45 +0200

    02 - Styling with emotion - use some css props

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,14 +1,14 @@
-// 🐨 make sure to add the comment and import jsx from @emotion/core
-// up here so you can use the css prop
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+import * as React from 'react'
 
 // 🐨 let's get a solid reset of global styles so everything looks a bit better
 // In this project we're using bootstrap-reboot which you can import from
-// bootstrap/dist/css/bootstrap-reboot.css
+import 'bootstrap/dist/css/bootstrap-reboot.css'
 // 🦉 Note: you can definitely use regular styles to style React apps
 // and using any modern toolchain will allow you to simply import the CSS file
 // but CSS-in-JS is generally easier to maintain.
 import '@reach/dialog/styles.css'
-import * as React from 'react'
 import ReactDOM from 'react-dom'
 // 🐨 you'll need to import some new components that you'll be creating
 // in this file
@@ -63,27 +63,27 @@ function App() {
         console.log('register', formData)
     }
 
-    // 🐨 this div could use a css prop to get its children rendered nicer
-    // 🎨
-    //    display: 'flex',
-    //    flexDirection: 'column',
-    //    alignItems: 'center',
-    //    justifyContent: 'center',
-    //    width: '100%',
-    //    height: '100vh',
     return (
-        <div>
+        <div
+            css={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                height: '100vh',
+            }}
+        >
             <Logo width="80" height="80"/>
             <h1>Bookshelf</h1>
             {/*
-        🐨 the two buttons are too close, let's space them out
-          🎨 apply this to the div right below
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
-            gridGap: '0.75rem',
-      */}
             {/* 🐨 And make sure to use the new Button component for all these buttons */}
-            <div>
+            <div
+            css={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+                gridGap: '0.75rem',
+            }}>
                 <Modal>
                     <ModalOpenButton>
                         <button variant="primary">Login</button>
