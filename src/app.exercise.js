6c121f8bc13aa4803813eed51d99c793af646cbd
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-14 06:30:14 +0200

    formatted and initialized

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -8,18 +8,18 @@ import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
 function App() {
-  // 🐨 useState for the user
+    // 🐨 useState for the user
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
+    // 🐨 create a login function that calls auth.login then sets the user
+    // 💰 const login = form => auth.login(form).then(u => setUser(u))
+    // 🐨 create a registration function that does the same as login except for register
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
+    // 🐨 create a logout function that calls auth.logout() and sets the user to null
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
+    // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
+    // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
 
-  return <UnauthenticatedApp />
+    return <UnauthenticatedApp/>
 }
 
 export {App}
