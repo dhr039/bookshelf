af6fc8b1d4db0f1921a53a9943c53276017dfa7b
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-16 00:25:20 +0200

    register/login/logout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import {loadDevTools} from './dev-tools/load'
+// import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
 
-loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-})
+// loadDevTools(() => {
+//   ReactDOM.render(<App />, document.getElementById('root'))
+// })
+ReactDOM.render(<App />, document.getElementById('root'))
