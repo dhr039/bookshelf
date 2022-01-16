7690334c40d5acc4cb919eabb679bd252c43d255
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-16 18:26:41 +0200

    Load the user's data on page load

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client.exercise';
 
 function App() {
     const [user, setUser] = React.useState(null)
@@ -12,12 +13,31 @@ function App() {
     const login = form => auth.login(form).then(u => setUser(u))
     const register = form => auth.register(form).then(u => setUser(u))
 
+    async function getUser() {
+        let user = null
+
+        const token = await auth.getToken()
+        if (token) {
+            const data = await client('me', {token})
+            user = data.user
+        }
+
+        return user
+    }
+
+    React.useEffect(() => {
+        getUser().then(u => {
+            setUser(u)
+        })
+    }, [])
+
     function logout() {
         auth.logout()
         setUser(null)
     }
 
-    return (user ? <AuthenticatedApp user={user} logout={logout}/> : <UnauthenticatedApp login={login} register={register}/>)
+    return (user ? <AuthenticatedApp user={user} logout={logout}/> :
+        <UnauthenticatedApp login={login} register={register}/>)
 }
 
 export {App}
