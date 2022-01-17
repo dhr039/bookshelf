52de8a55476c3febed8e121e5e8c2de256b7a415
Dmitry Hristov <dumitru.hristov@muume.com>
2022-01-17 09:26:23 +0200

    automatically logout on 401

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,4 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(endpoint, {token, headers: customHeaders, ...customConfig} = {}) {
@@ -10,6 +11,15 @@ function client(endpoint, {token, headers: customHeaders, ...customConfig} = {})
     }
 
     return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+        if(response.status === 401) {
+            await auth.logout()
+
+            /*trigger a full page refresh to wipe any data that is in the memory:*/
+            window.location.assign(window.location)
+
+            /*for sake of completeness reject the promise (even  thought there will be a refresh anyway):*/
+            return Promise.reject({message: 'please re-login'})
+        }
         const data = await response.json()
         if (response.ok) {
             return data
